024ccddf7e01e67dfa7c0987f03e44119f7996ad
ksaqib <ksaqib.ks@gmail.com>
2020-01-21 02:06:34 -0600

    HTML5 AND CSS3 COURSE PAGE 67 [Resubmission]

---
+++ HTML documents/Hello_World.js
@@ -0,0 +1,6 @@
+alert("Starting Javasctipt!");
+function firstFunc(){
+		let string =  "I eat Fish, you must kiss me crazy.";
+		let result = string.fontcolor("Green");
+		document.getElementById("fish").innerHTML = result;
+	}
